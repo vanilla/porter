c27ef252091943fe47a1b48302f36e278402a29a
Javier-Rotelli <javier.rotelli@gmail.com>
2020-04-22 00:15:53 -0300

    add Attachments support to mybb importer

--- packages/mybb.php
+++ packages/mybb.php
@@ -18,6 +18,7 @@ $supported['mybb']['features'] = array(
     'Passwords' => 1,
     'Avatars' => 1,
     'Bookmarks' => 1,
+    'Attachments' => 1,
 );
 
 class MyBB extends ExportController {
@@ -127,6 +128,28 @@ class MyBB extends ExportController {
             FROM_UNIXTIME(dateline) as DateInserted,
             'BBCode' as Format
          from :_posts p", $comment_Map);
+
+        // Media
+        $media_Map = array(
+            'aid' => 'MediaID',
+            'pid' => 'ForeignID',
+            'uid' => 'InsertUserId',
+            'filesize' => 'Size',
+            'filename' => 'Name',
+            'height' => 'ImageHeight',
+            'width' => 'ImageWidth',
+            'filetype' => 'Type',
+            'thumb_width' => array('Column' => 'ThumbWidth', 'Filter' => array($this, 'filterThumbnailData')),
+        );
+        $ex->exportTable('Media', "
+            select a.*,
+                600 as thumb_width,
+                concat('attachments/', a.thumbnail) as ThumbPath,
+                concat('attachments/', a.attachname) as Path,
+                'Comment' as ForeignTable
+            from :_attachments a
+            where a.pid > 0
+        ", $media_Map);
 
         // UserDiscussion.
         $userDiscussion_Map = array(
